3ae61a7106289de30eb6a61dc042c7c4245cee8b
shriyasharma4484 <shriyasharma4484@gmail.com>
2026-01-31 00:07:35 +0530

    27/01/26

---
+++ Multi.java
@@ -0,0 +1,27 @@
+// class Grandfather {
+//     void showGF() {
+//         System.out.println("this is grand father");
+//     }
+// }
+
+// class Parent extends Grandfather {
+//     void showP() {
+//         System.out.println("this is parent class");
+//     }
+// }
+
+// class Child extends Parent {
+//     void showX() {
+//         System.out.println("this is child class");
+//     }
+// }
+
+// class Multi {
+//     public static void main(String[] args) {
+//         Child c = new Child();
+//         c.showGF();
+//         c.showP();
+//         c.showX();
+//     }
+// }
+
