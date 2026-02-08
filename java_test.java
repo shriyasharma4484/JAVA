ddcb6fd4666cbab258976cef35361a73cca99afd
shriyasharma4484 <shriyasharma4484@gmail.com>
2026-02-08 10:52:37 +0530

    java_test

---
+++ java_test.java
@@ -0,0 +1,19 @@
+// public class java_test{
+//     public static void main(String[]args){
+//         String fname="Shriya";
+//         System.out.println("\t");
+//         String Iname="Sharma";
+//         String fullName=fname+Iname;
+//         System.out.println(fullName);
+//     }
+// }
+
+public class java_test{
+    public static void main(String[]args){
+        double num=10.5d;
+        int myNum=(int)num;
+        System.out.println(num);
+        System.out.println(myNum);
+
+    }
+}
